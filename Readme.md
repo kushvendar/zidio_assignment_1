c6e2f42bacce2eb9a01a70838d7a0d3dd1b65116
Kush <jangidkush1234@gmail.com>
2025-05-01 21:56:58 +0530

    boiler plate set up

---
+++ Readme.md
@@ -0,0 +1,3 @@
+ step:-1 if you are working on frontend create a seperate .jsx file in components folder and import it in App.jsx
+      
+step:-2 If you are a backend engineer work on server file 
---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div className=''>
+      if you are seeing this means you have set up project correctly
+      how to proceed go to readme 
+     
+    </div>
+  )
+}
+
+export default App
